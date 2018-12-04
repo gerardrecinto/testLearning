c9a352cce3a1c9ca397412129d0fdf51f2e88b4c
gerardrecinto <gerardrecinto@gmail.com>
2018-12-03 17:22:59 -0800

    windows2

--- src/test/java/gerard/base/TestBase.java
+++ src/test/java/gerard/base/TestBase.java
@@ -1,6 +1,7 @@
 package gerard.base;
 
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -29,47 +30,51 @@ public class TestBase {
     public static Properties config = new Properties();
     public static Properties OR = new Properties();
     public static FileInputStream fis;
+    public static Logger log = Logger.getLogger("devpinoyLogger");
     @BeforeSuite
     public void setUp(){
-        FileInputStream fis;
-        try {
-            fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\gerard\\resources\\properties\\Config.properties");
-            try{
-                config.load(fis);
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-
-
-
-        try {
-            fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\gerard\\resources\\properties\\OR.properties");
+        if(driver==null) {
+            FileInputStream fis;
             try {
-                OR.load(fis);
-            } catch (IOException e){
+                fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\gerard\\resources\\properties\\Config.properties");
+                try {
+                    config.load(fis);
+                    log.debug("Config loaded!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-        } catch (FileNotFoundException e){
-            e.printStackTrace();
+
+
+            try {
+                fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\java\\gerard\\resources\\properties\\OR.properties");
+                try {
+                    OR.load(fis);
+                    log.debug("OR loaded!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+
+            //TODO later
+            if (config.getProperty("browser").equals("firefox")) {
+                System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\gerard\\resources\\executables\\geckodriver.exe");
+                driver = new FirefoxDriver();
+            } else if (config.getProperty(("browser")).equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\java\\gerard\\resources\\executables\\chromedriver.exe");
+                driver = new ChromeDriver();
+            } else if (config.getProperty("browser").equals("ie")) {
+                System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\src\\test\\java\\gerard\\resources\\executables\\IEDriverServer.exe");
+            }
+
         }
-
-
-        //TODO later
-        if (config.getProperty("browser").equals("firefox")){
-            System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\src\\test\\gerard\\resources\\executables\\geckodriver.exe");
-            driver = new FirefoxDriver();
-        } else if (config.getProperty(("browser")).equals("chrome")){
-            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")  + "\\src\\test\\java\\gerard\\resources\\executables\\chromedriver.exe");
-            driver = new ChromeDriver();
-        } else if (config.getProperty("browser").equals("ie")){
-            System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\src\\test\\java\\gerard\\resources\\executables\\IEDriverServer.exe");
-        }
-
-
         driver.get(config.getProperty("testsiteurl"));
+        log.debug("Navigated to " + config.getProperty("testsiteurl"));
         driver.manage().window().fullscreen();
         driver.manage().timeouts().implicitlyWait(Integer.parseInt(config.getProperty("implicit.wait")), TimeUnit.SECONDS);
 
--- src/test/java/gerard/testcases/LoginTest.java
+++ src/test/java/gerard/testcases/LoginTest.java
@@ -8,6 +8,7 @@ public class LoginTest extends TestBase {
 
     @Test
     public void loginAsBankManager() throws InterruptedException {
+        log.debug("Inside Login Test");
         driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
         Thread.sleep(3000);
     }
