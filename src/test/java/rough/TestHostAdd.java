a8ff8bb936839cca655c7ced0c944746132ce237
gerardrecinto <gerardrecinto@gmail.com>
2018-12-28 15:56:27 -0800

    finished project 1 finally

---
+++ src/test/java/rough/TestHostAdd.java
@@ -0,0 +1,24 @@
+package rough;
+
+import gerard.resources.utilities.MonitoringMail;
+import gerard.resources.utilities.TestConfig;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class TestHostAdd {
+
+	public static void main(String[] args) throws UnknownHostException, AddressException, MessagingException {
+
+		MonitoringMail mail = new MonitoringMail();
+		String messageBody = "http://" + InetAddress.getLocalHost().getHostAddress()
+				+ ":8080/job/DataDrivenLiveProject/Extent_Reports/";
+		System.out.println(messageBody);
+		
+		mail.sendMail(TestConfig.server, TestConfig.from, TestConfig.to, TestConfig.subject, messageBody);
+
+	}
+
+}
